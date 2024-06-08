1bc354e5650fc644796928f231fb01b5418303af
jjrivilla <fuanhc25@gmail.com>
2024-06-07 22:53:47 -0500

    Basicos de Program

---
+++ main.py
@@ -0,0 +1,45 @@
+print("Hola mundo")
+
+#enteros
+"""
+print(5+5)
+print(6-3)
+print(100/20)
+print(100//17)
+print(5*25)
+print(5**2)
+"""
+
+
+#string
+
+"""
+print("hola")
+print("Buenas")
+print("Tardes")
+print()
+print()
+print()
+"""
+
+#boleanos
+#True
+#False
+
+"""
+print(3 > 5)
+print(2 < 0)
+print(5 > 5)
+"""
+
+
+#operadores ternarios
+#hola == 3
+
+#print(hola)
+
+
+
+
+
+
