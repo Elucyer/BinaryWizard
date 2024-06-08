80e415f5e11c7ac74fa1c30cff7b3cbd8cf50047
Elucyer <otoshimoto@gmail.com>
2024-06-07 22:53:33 -0500

    Basicos de programacion

---
+++ main.py
@@ -0,0 +1,40 @@
+###print("Hola Mundo")
+
+#### Tipos ###
+
+#  Enteros
+print(5+5)
+print(6-3)
+print(100/20)  #------> float
+print(100//17)
+print(5*25)
+print(5**2)
+
+#  String
+
+print('Hola')
+print('buenas')
+print('tardes')
+
+#  Boleanos
+True
+False
+
+print(3 > 5)
+print(2 < 0)
+print(5 > 3)
+
+hola = 3       #------------> Defino una variable con un solo =
+hola_1 = "3" == 3
+hola_1 = 1 != 3
+print(hola_1)
+
+
+Mi_primer_variable = 3*8   #----------> Entero
+print(Mi_primer_variable)
+
+comparacion = Mi_primer_variable != 10 #-----> boleano
+print(comparacion)
+
+logica = comparacion > 9
+print(logica)
