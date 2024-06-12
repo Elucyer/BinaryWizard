3290dd85f2c6f6f49be57a0c5d94984e17681131
jjrivilla <fuanhc25@gmail.com>
2024-06-11 22:59:25 -0500

    Se crea archivo de listas

---
+++ ejerciosLista.py
@@ -0,0 +1,95 @@
+"""
+1)Se debe devolver la miltiplicacion de los numero del 1 al 9
+2)Se debe imprimir la lista en pantalla y solo debe aparecer la palabra "imprimir"
+3)Se debe imprimir en pantalla la combina de palabras "Hola", "Soy Janer" y tengo 24 anos, jugando con los valores de la lista 
+4)Se debe ordenar la lista de mayor a menor eliminando los valores string
+--5)Se debe elminar listar el total de valores que contiene la lista
+6)Se debe realizar al menos una operacion basica de cada operacion con los valores de la lista
+7)Se debe separar los elementos strign de los elementos enteros y mostrarlos en pantalla en dos listas diferentes
+
+"""
+
+Mi_Lista = [4,3,2,1,6,5,7,5,"Hola","Soy Juan","Imprimir", 4,9,10,8,14,20]
+
+
+
+"""
+#solucion 6
+Mi_ListaEnteros1=Mi_Lista[0:8]
+Mi_ListaEnteros2=Mi_Lista[11:17]
+
+Mi_ListaEnterosDef = Mi_ListaEnteros1+ Mi_ListaEnteros2
+print(Mi_ListaEnterosDef[0] + Mi_ListaEnterosDef[1])
+print(Mi_ListaEnterosDef[2] + Mi_ListaEnterosDef[3])
+print(Mi_ListaEnterosDef[4] + Mi_ListaEnterosDef[5])
+print(Mi_ListaEnterosDef[6] + Mi_ListaEnterosDef[7])
+print(Mi_ListaEnterosDef[8] + Mi_ListaEnterosDef[9])
+print(Mi_ListaEnterosDef[10] + Mi_ListaEnterosDef[11])
+print(Mi_ListaEnterosDef[12] + Mi_ListaEnterosDef[13])
+
+print(Mi_ListaEnterosDef[0] - Mi_ListaEnterosDef[1])
+print(Mi_ListaEnterosDef[2] - Mi_ListaEnterosDef[3])
+print(Mi_ListaEnterosDef[4] - Mi_ListaEnterosDef[5])
+print(Mi_ListaEnterosDef[6] - Mi_ListaEnterosDef[7])
+print(Mi_ListaEnterosDef[8] - Mi_ListaEnterosDef[9])
+print(Mi_ListaEnterosDef[10] - Mi_ListaEnterosDef[11])
+print(Mi_ListaEnterosDef[12] - Mi_ListaEnterosDef[13])
+
+print(Mi_ListaEnterosDef[0] * Mi_ListaEnterosDef[1])
+print(Mi_ListaEnterosDef[2] * Mi_ListaEnterosDef[3])
+print(Mi_ListaEnterosDef[4] * Mi_ListaEnterosDef[5])
+print(Mi_ListaEnterosDef[6] * Mi_ListaEnterosDef[7])
+print(Mi_ListaEnterosDef[8] * Mi_ListaEnterosDef[9])
+print(Mi_ListaEnterosDef[10] * Mi_ListaEnterosDef[11])
+print(Mi_ListaEnterosDef[12] * Mi_ListaEnterosDef[13])
+
+print(Mi_ListaEnterosDef[0] / Mi_ListaEnterosDef[1])
+print(Mi_ListaEnterosDef[2] / Mi_ListaEnterosDef[3])
+print(Mi_ListaEnterosDef[4] / Mi_ListaEnterosDef[5])
+print(Mi_ListaEnterosDef[6] / Mi_ListaEnterosDef[7])
+print(Mi_ListaEnterosDef[8] / Mi_ListaEnterosDef[9])
+print(Mi_ListaEnterosDef[10] / Mi_ListaEnterosDef[11])
+print(Mi_ListaEnterosDef[12] / Mi_ListaEnterosDef[13])
+"""
+
+
+
+
+"""
+print("solucion 2")
+print(Mi_Lista[10])
+"""
+
+"""
+print("solucion 3")
+mi_ListaString = Mi_Lista[8:10]
+mi_ListaString.append("Tengo 24 años")
+print(mi_ListaString)
+"""
+
+
+"""
+print("solucion 5")
+print(Mi_Lista.clear())
+"""
+
+
+
+
+
+
+"""
+print("solucion 7")
+mi_ListaString = Mi_Lista[-9:-6]
+print(mi_ListaString)
+
+mi_ListaEnteros1 = Mi_Lista[0:8]
+mi_ListaEnteros2 = Mi_Lista[11:17]
+mi_ListaEnterosDefinitivo = mi_ListaEnteros1 + mi_ListaEnteros2
+print(mi_ListaEnterosDefinitivo)
+"""
+
+
+
+
+
--- main.py
+++ main.py
@@ -38,8 +38,55 @@ print(5 > 5)
 
 #print(hola)
 
+#listas
+
+#mi_lista = [1, "d", True, 12, 244.221]
+
+#print(mi_lista)
 
 
+mi_lista = [1,2,3,4,5,6,7,8,9, 10343, 1,2,3,4,5,6]
+
+enteros = [15,235,34534,234456]
+
+mi_lista2 = ["hola", "me", "llamo", "juan",]
+
+
+#indices en las listas inician desde 0, 1, 2, 3, 4....N
+
+"""
+print(mi_lista[0]) #0 es el valor  del indice que en este caso es 1
+
+print(mi_lista[-9])
+
+print(len(mi_lista)) #len() es para contar la cantidad de valores que hay en la lista
+
+print(max(mi_lista)) #max() imprime el valor maximo
+
+print(min(mi_lista)) #min() imprime el valor minimo de la lista
+
+"""
+
+#mi_lista.extend(enteros) #extend() agrega datos a la lista deseada
+
+#print(mi_lista)
+
+#mi_lista.append() #append() agrega en la ultima posicion
+
+#print(mi_lista)
+
+
+#print(mi_lista.index(4))
+
+#mi_lista.pop() #pop() elimina el indice de la lista
+#print(mi_lista)
+
+#print(mi_lista.clear()) #clear() borra los valores de la lista
+
+#mi_lista.sort() #sort() organiza de menor a mayor los valores de una lista
+#mi_lista.reverse() #reverse() organiza de mayor a menor
+
+#mi_listaV2 = lista(set(mi_lista)) #set es para elimninar valores duplicados
 
 
 
